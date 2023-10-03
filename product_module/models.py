29e3b7c43fa2590e730d93c396eb46763d219d37
mint <hamedsafikhani854@gmail.com>
2023-10-03 22:59:09 +0330

    Dynamic url for product with two way (url and reverse) - lesson 42

--- product_module/models.py
+++ product_module/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator,MaxValueValidator
+from django.urls import reverse
 # Create your models here.
 
 
@@ -9,6 +10,9 @@ class Product(models.Model):
     rating = models.IntegerField(default=0,validators=[MinValueValidator(1),MaxValueValidator(5)])
     short_description = models.CharField(max_length=360,null=True)
     is_active = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse('product_detail',args=[self.id])
 
 
     def __str__(self):
