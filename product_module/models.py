3095683638876a5d753c61bafd81f70601f683ae
mint <hamedsafikhani854@gmail.com>
2023-10-03 13:42:10 +0330

    change db - lesson 36

--- product_module/models.py
+++ product_module/models.py
@@ -1,9 +1,16 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator,MaxValueValidator
 # Create your models here.
 
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
     price = models.IntegerField()
+    rating = models.IntegerField(default=0,validators=[MinValueValidator(1),MaxValueValidator(5)])
+    short_description = models.CharField(max_length=360,null=True)
+    is_active = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return f"title : {self.title} , Price : {self.price} "
 
