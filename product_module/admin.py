8403186ccd4d809e3fe253bbafbaeeba08e3af88
mint <hamedsafikhani854@gmail.com>
2023-10-04 13:59:10 +0330

    admin page customize admin prepopulated_fields - lesson 47

--- product_module/admin.py
+++ product_module/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        'slug' : ['title']
+    }
 
-admin.site.register(models.Product)
+admin.site.register(models.Product,ProductAdmin)
