6d82322e7c80ec0c3fb3b5df8c952c80bb4f9c14
mint <hamedsafikhani854@gmail.com>
2023-10-04 12:49:26 +0330

    aggregate and count and avg and order_by - lesson 44-45

--- product_module/views.py
+++ product_module/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Product
+from django.db.models import Avg
 # Create your views here.
 
 
 def product_list(request):
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('-price')
+    number_of_products = products.count()
+    avg_of_rating = products.aggregate(Avg('rating')) # aggregate( avg , min , max and ... )
+
     return render(request,'product_module/product_list.html',context={
-        "products":products
+        "products":products,
+        "number_of_products" : number_of_products,
+        "avg_of_rating" : avg_of_rating
     })
 
 
