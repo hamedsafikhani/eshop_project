1ba37b0663aa8e1259e9cf2d14ca3eb6346d1edc
mint <hamedsafikhani854@gmail.com>
2023-10-05 22:13:31 +0330

    "CHANGE MODEL AND CREATE NEW DB FOR NEW PURPOSE" LESSON 58

--- product_module/admin.py
+++ product_module/admin.py
@@ -1,21 +1,8 @@
-# user admin pass admin123
 from django.contrib import admin
 from . import models
 
-
 # Register your models here.
 
-class ProductAdmin(admin.ModelAdmin):
-    prepopulated_fields = {
-        'slug': ['title']
-    }
-
-    list_display = ['title', 'price', 'rating', 'is_active', 'category']
-    list_filter = ['rating', 'is_active']
-    list_editable = ['price', 'rating', 'is_active', 'category']
-
-
-admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.Product)
 admin.site.register(models.ProductCategory)
-admin.site.register(models.ProductInformation)
-admin.site.register(models.ProductTags)
+admin.site.register(models.ProductTag)
--- product_module/models.py
+++ product_module/models.py
@@ -1,78 +1,59 @@
 from django.db import models
-from django.core.validators import MinValueValidator,MaxValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
+
+
 # Create your models here.
 
-class ProductTags(models.Model):
-    tag = models.CharField(max_length=300,verbose_name='عنوان')
-    def __str__(self):
-        return self.tag
-    class Meta:
-        verbose_name = 'تگ محصول'
-        verbose_name_plural = 'تگ های محصولات'
 class ProductCategory(models.Model):
-    title = models.CharField(max_length=300,verbose_name='عنوان')
-    url_title = models.CharField(max_length=300,verbose_name='عنوان url')
+    title = models.CharField(max_length=300, db_index=True, verbose_name='عنوان')
+    url_title = models.CharField(max_length=300, db_index=True, verbose_name='عنوان در url')
+    is_active = models.BooleanField(verbose_name='فعال / غیرفعال')
+    is_delete = models.BooleanField(verbose_name='حذف شده / نشده')
 
     def __str__(self):
-        return self.title
+        return f'( {self.title} - {self.url_title} )'
 
     class Meta:
         verbose_name = 'دسته بندی'
-        verbose_name_plural =  'دسته بندی ها'
+        verbose_name_plural = 'دسته بندی ها'
 
-class ProductInformation(models.Model):
-    color = models.CharField(max_length=10,verbose_name='رنگ')
-    size = models.CharField(max_length=10,verbose_name='سایز')
 
-    def __str__(self):
-        return f'{self.color}-{self.size}'
-    class Meta:
-        verbose_name = 'اطلاعات محصول'
-        verbose_name_plural = 'تمامی اطلاعات محصول'
 class Product(models.Model):
-    title = models.CharField(max_length=100)
-    category = models.ForeignKey(
+    title = models.CharField(max_length=300)
+    category = models.ManyToManyField(
         ProductCategory,
-        on_delete=models.CASCADE,
-        null=True,
-        related_name='products'
-    )
-    product_tags = models.ManyToManyField(ProductTags,verbose_name='تگ های محصول')
-    product_information = models.OneToOneField(
-        ProductInformation,
-        on_delete=models.CASCADE,
-        related_name='product_information',
-        verbose_name='اطلاعات تکمیلی',
-        null=True,blank=True)
-    price = models.IntegerField()
-    rating = models.IntegerField(
-        default=0,
-        validators=[MinValueValidator(1),MaxValueValidator(5)]
-    )
-    short_description = models.CharField(
-        max_length=360,
-        null=True
-    )
-    is_active = models.BooleanField(default=False)
-    slug = models.SlugField(
-        default='',
-        null=False,
-        db_index=True
-    ) # db_index = True : busy field for speed queries to DB
+        related_name='product_categories',
+        verbose_name='دسته بندی ها')
+    price = models.IntegerField(verbose_name='قیمت')
+    short_description = models.CharField(max_length=360, db_index=True, null=True, verbose_name='توضیحات کوتاه')
+    description = models.TextField(verbose_name='توضیحات اصلی', db_index=True)
+    slug = models.SlugField(default="", null=False, db_index=True, blank=True, max_length=200, unique=True)
+    is_active = models.BooleanField(default=False, verbose_name='فعال / غیرفعال')
+    is_delete = models.BooleanField(verbose_name='حذف شده / نشده')
 
     def get_absolute_url(self):
-        return reverse('product_detail',args=[self.slug])
+        return reverse('product-detail', args=[self.slug])
 
-    def save(self,*args,**kwargs):
+    def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
-        super().save(*args,**kwargs)
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.title}"
+        return f"{self.title} ({self.price})"
 
     class Meta:
         verbose_name = 'محصول'
         verbose_name_plural = 'محصولات'
 
+
+class ProductTag(models.Model):
+    caption = models.CharField(max_length=300, db_index=True, verbose_name='عنوان')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_tags')
+
+    class Meta:
+        verbose_name = 'تگ محصول'
+        verbose_name_plural = 'تگ های محصولات'
+
+    def __str__(self):
+        return self.caption
