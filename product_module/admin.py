93d626f13d619e8ecb9da0598348aa027c9a21de
mint <hamedsafikhani854@gmail.com>
2023-10-09 20:55:33 +0330

    some feature to admin - lesson 59

--- product_module/admin.py
+++ product_module/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+class productAdmin(admin.ModelAdmin):
+    list_filter = ['category','is_active','is_delete']
+    list_display = ['title','price','is_active','is_delete']
+    list_editable = ['price','is_active']
 
-admin.site.register(models.Product)
+admin.site.register(models.Product,productAdmin)
 admin.site.register(models.ProductCategory)
 admin.site.register(models.ProductTag)
