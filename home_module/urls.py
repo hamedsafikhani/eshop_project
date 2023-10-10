c92b98448be360a7c511f1fc648b026261c118f0
mint <hamedsafikhani854@gmail.com>
2023-10-11 00:46:46 +0330

    parse _layout.html with render_partial and include tags with views name way - lesson 61

--- home_module/urls.py
+++ home_module/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index)
+    path('',views.index),
+    path('contact-us',views.contact_page)
+    # path('site-header-component',views.site_header_component) # yeki az rah ha baraye render partial
 ]
