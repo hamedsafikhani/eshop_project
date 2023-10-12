3ad86fbbead285371540d909a2adf8be8870ef3b
mint <hamedsafikhani854@gmail.com>
2023-10-12 19:39:12 +0330

    get post data & crf token task  - lesson 66

--- product_module/urls.py
+++ product_module/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.product_list,name=''),
+    path('',views.product_list,name='product_list'),
     path('<slug:slug>',views.product_detail,name="product_detail"),
 ]
