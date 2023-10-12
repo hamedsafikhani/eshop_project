bf11184dbf71a6f7029c22f7d278d4a0fcaa96e0
mint <hamedsafikhani854@gmail.com>
2023-10-12 21:34:34 +0330

    about forms in django  - lesson 68

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -2,8 +2,14 @@ from django import forms
 
 
 
-class ContactUsForm(forms.Form):
-    full_name = forms.CharField(label='نام و نام خانوادگی')
-    email = forms.EmailField()
-    subject = forms.CharField()
-    text = forms.CharField()
+class ContactUsForm(forms.Form): # django forms fields
+    full_name = forms.CharField(
+        label='نام و نام خانوادگی',
+        max_length=50,
+        error_messages={
+            "required" : 'لطفا نام را وارد کنید'
+        }
+    )
+    email = forms.EmailField(label='ایمیل',widget=forms.EmailInput)
+    subject = forms.CharField(label='عنوان')
+    text = forms.CharField(label='متن پیام',widget=forms.Textarea)
