6181410b93b9115f0e3b2158e6fdb30f7f7a8f37
mint <hamedsafikhani854@gmail.com>
2023-10-14 21:04:57 +0330

    class base view - View and TemplateView - lesson 72

--- home_module/urls.py
+++ home_module/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index,name='home_page'),
-    path('contact-us',views.contact_page)
-    # path('site-header-component',views.site_header_component) # yeki az rah ha baraye render partial
+    path('',views.HomeView.as_view(),name='home_page'),
 ]
