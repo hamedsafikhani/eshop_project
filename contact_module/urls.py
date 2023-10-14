c4d863b221575ed89933741f0068d35f3378319f
mint <hamedsafikhani854@gmail.com>
2023-10-14 21:04:57 +0330

    class base view - View and TemplateView - lesson 72

--- contact_module/urls.py
+++ contact_module/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.contact_page,name='contact_us_page')
+    path('',views.ContactUsView.as_view(),name='contact_us_page')
 ]
